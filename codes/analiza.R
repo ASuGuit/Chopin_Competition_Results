a071525e84799add3c19348d3d80f28683dd29b3
ASuGuit <adamstruzikguit@gmail.com>
2025-10-31 19:50:08 +0100

    results uploaded

---
+++ codes/analiza.R
@@ -0,0 +1,6 @@
+library(readr)
+
+wyniki_1 <- read_csv("data-raw/Chopin_wyniki_1.csv")
+wyniki_2 <- read_csv("data-raw/Chopin_wyniki_2.csv")
+wyniki_3 <- read_csv("data-raw/Chopin_wyniki_3.csv")
+wyniki_4 <- read_csv("data-raw/Chopin_wyniki_4.csv")
